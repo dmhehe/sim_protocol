0eff8eeb41b534fe35d258e48a035e6d5b7ae9c1
dmhehe <847990303@qq.com>
2024-08-10 14:39:21 +0800

    Update player_101.py

--- protocol/player_101.py
+++ protocol/player_101.py
@@ -1,29 +1,31 @@
 
-from protocol.base import *
+# from protocol.base import *
+from base import *
 
+#  i是64位整形  s是字符串
 
 
 S2C_PLAYER = 101
 
-class S2CPlayerInfo:
-    tid = D(INT_16, S2C_PLAYER)
-    subtid = D(INT_16, 1)
-    name = D(STR)
-    grade = D(INT_32)
+#S2C玩家信息             父协议， 子协议
+class S2CPlayerInfo(S2C(S2C_PLAYER, 1)):
+    name = s   #名字
+    grade = i  #等级
 
-class S2COneInfo:
-    tid = D(INT_16, S2C_PLAYER)
-    subtid = D(INT_16, 2)
-    attname = D(STR)
-
+#玩家的一个属性            父协议， 子协议
+class S2COneInfo(S2C(S2C_PLAYER, 2)):
+    attname = s #属性名字
+    val = i   #属性值
 
 
+
+#客户端发给服务端
 C2S_PLYAER = 101
 
-class C2SGetOneInfo:
-    tid = D(INT_16, C2S_PLYAER)
-    subtid = D(INT_16, 1)
-    attname = D(STR)
+#S2C玩家信息            父协议， 子协议
+class C2SGetOneInfo(C2S(C2S_PLYAER, 1)):
+    attname = s  #属性名字
+
     
     
     
