560a0970c28895d3d3715f3be78f3bc495bf2571
dmhehe <847990303@qq.com>
2025-07-10 16:57:36 +0800

    11111

--- sendpublic.py
+++ sendpublic.py
@@ -29,22 +29,20 @@ STR_LIST = 107
 def PackFunc(json_list, data):
     for i in json_list:
         itype = i[0]
+        name = i[1]
         if itype == OBJ:
-            name = json_list[1]
             new_data = data[name]
-            PackFunc(json_list[2], new_data)
+            PackFunc(i[2], new_data)
             
         elif itype == OBJ_LIST:
-            name = json_list[1]
             new_data = data[name]
             PackInt(len(new_data))
             for item in new_data:
-                PackFunc(json_list[2], item)
+                PackFunc(i[2], item)
         elif itype == INT_16:
             new_data = data[name]
             PackInt(new_data)
         elif itype == INT_16_LIST:
-            name = json_list[1]
             new_data = data[name]
             PackInt(len(new_data))
             for item in new_data:
@@ -53,7 +51,6 @@ def PackFunc(json_list, data):
             new_data = data[name]
             PackInt(new_data)
         elif itype == INT_32_LIST:
-            name = json_list[1]
             new_data = data[name]
             PackInt(len(new_data))
             for item in new_data:
@@ -62,16 +59,14 @@ def PackFunc(json_list, data):
             new_data = data[name]
             PackInt(new_data)
         elif itype == INT_64_LIST:
-            name = json_list[1]
             new_data = data[name]
             PackInt(len(new_data))
             for item in new_data:
                 PackInt(item)
-        elif itype == str:
+        elif itype == STR:
             new_data = data[name]
             PackString(new_data)
         elif itype == STR_LIST:
-            name = json_list[1]
             new_data = data[name]
             PackInt(len(new_data))
             for item in new_data:
@@ -95,3 +90,80 @@ def doSendData(pid: int, mid1: int, mid2: int, data):
     PackInt(mid2)
     PackFunc(json_list, data)
     PacketSend(pid)
+
+
+
+
+
+
+
+def UnPackFunc(json_list):
+    data = {}
+    for i in json_list:
+        itype = i[0]
+        name = i[1]
+        if itype == OBJ:
+            data[name] = UnPackFunc(i[2])
+            
+        elif itype == OBJ_LIST:
+            ilen = UnPackInt()
+            if (ilen > 1000):
+                raise ValueError(f"List length {ilen} exceeds maximum limit of 1000")
+            new_data = []
+            data[name] = new_data
+            for j in range(ilen):
+                item = UnPackFunc(i[2])
+                new_data.append(item)
+                
+        elif itype == INT_16:
+            data[name] = UnPackInt()
+        elif itype == INT_16_LIST:
+            ilen = UnPackInt()
+            if (ilen > 1000):
+                raise ValueError(f"List length {ilen} exceeds maximum limit of 1000")
+            new_data = []
+            data[name] = new_data
+            for j in range(ilen):
+                item = UnPackInt()
+                new_data.append(item)
+        elif itype == INT_32:
+            data[name] = UnPackInt()
+        elif itype == INT_32_LIST:
+            ilen = UnPackInt()
+            if (ilen > 1000):
+                raise ValueError(f"List length {ilen} exceeds maximum limit of 1000")
+            new_data = []
+            data[name] = new_data
+            for j in range(ilen):
+                item = UnPackInt()
+                new_data.append(item)
+        elif itype == INT_64:
+            data[name] = UnPackInt()
+        elif itype == INT_64_LIST:
+            ilen = UnPackInt()
+            if (ilen > 1000):
+                raise ValueError(f"List length {ilen} exceeds maximum limit of 1000")
+            new_data = []
+            data[name] = new_data
+            for j in range(ilen):
+                item = UnPackInt()
+                new_data.append(item)
+        elif itype == STR:
+            data[name] = UnPackString()
+        elif itype == STR_LIST:
+            ilen = UnPackInt()
+            if (ilen > 1000):
+                raise ValueError(f"List length {ilen} exceeds maximum limit of 1000")
+            new_data = []
+            data[name] = new_data
+            for j in range(ilen):
+                item = UnPackString()
+                new_data.append(item)
+
+    return data
+
+def doUnpackData(pid:int, mid1: int, mid2: int):
+    json_list = pydata.s2c.g_C2S.get(mid1, {}).get(mid2)
+    
+    data = UnPackFunc(json_list)
+    return data
